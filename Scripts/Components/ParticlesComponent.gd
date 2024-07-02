ecd32996e457add208804486e996b0d3b52a9cb3
Jan <93995100+Janfra@users.noreply.github.com>
2024-07-02 19:31:56 +0100

    Bullet particles (#4)
    * Deleting unnecessary scenes
    
    * Bullet particles, Lifetime & Adjustment

---
+++ Scripts/Components/ParticlesComponent.gd
@@ -0,0 +1,36 @@
+class_name ParticlesComponent
+extends Node
+
+## Handles the logic for particles emittion and lifetime
+
+@export_category("Dependencies")
+@export var target: NotifiedNode3D
+@export var particles: GPUParticles3D
+
+@export_category("Configuration")
+@export var is_auto_play: bool
+@export var duration_type: ParticleLifetimeDuration
+@export var duration: float
+
+enum ParticleLifetimeDuration
+{
+	TargetLifetime,
+	Duration
+}
+
+func _ready():
+	if is_auto_play:
+		emit_particles()
+	
+	if duration_type == ParticleLifetimeDuration.TargetLifetime:
+		target.freed.connect(stop_emittion.bind())
+		target.lifetime = particles.lifetime
+		
+
+func emit_particles() -> void:
+	particles.emitting = true
+	
+
+func stop_emittion() -> void:
+	particles.emitting = false
+	
