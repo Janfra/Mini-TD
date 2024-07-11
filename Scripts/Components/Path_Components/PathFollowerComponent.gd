663347aedf9159e0fedb28a809d2b85a8646784d
Janfra <janfrancoherrera@gmail.com>
2024-07-11 22:54:35 +0100

    Currency Component & Null Checking

--- Scripts/Components/Path_Components/PathFollowerComponent.gd
+++ Scripts/Components/Path_Components/PathFollowerComponent.gd
@@ -2,7 +2,7 @@ class_name PathFollowerComponent
 extends Node
 
 @export_category("Node To Move")
-@export var target: Node3D
+@export var _target: Node3D
 
 @export_category("Configuration")
 @export var speed: float
@@ -12,9 +12,17 @@ var _distance_traveled: float
 func update_distance_traveled(path_handler : EnemyPathComponent, delta : float):
 	_distance_traveled += speed * delta
 	var path_data: EnemyPathComponent.PathTravelData = path_handler.get_position_data_along_path(_distance_traveled)
-	target.global_position = path_data.position
+	_target.global_position = path_data.position
 	
 	if path_data.is_path_completed:
 		print("Completed path")
 		path_handler.remove_path_follower_listener(self)
+	
+
+func set_path_follower(set_target : Node3D) -> void:
+	if _target and not (_target == set_target):
+		print("Changed path follower component target from %s to %s" % [_target.name, set_target.name])
+		
+	
+	_target = set_target
 	
